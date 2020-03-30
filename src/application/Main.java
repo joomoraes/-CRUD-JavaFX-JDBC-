299aa6520c505e513fb5d240692e987da81722f4
Joao Pedro <52724220+joomoraes@users.noreply.github.com>
2020-03-30 18:54:58 -0300

    Desenvolvimento da troca de telas (erro na lib do jdk)

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,9 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -27,7 +30,12 @@ public class Main extends Application {
 		}
 	}
 
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
 }
