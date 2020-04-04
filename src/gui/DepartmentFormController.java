14cc8b9cfa54a43ab7733b2f1d93fa025f7b5196
Joao Pedro <52724220+joomoraes@users.noreply.github.com>
2020-04-03 23:56:06 -0300

    Foram adicionados as exceções em formulário, para tratamento de possíveis erros

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentServices;
 
 public class DepartmentFormController implements Initializable{
@@ -66,12 +69,17 @@ public class DepartmentFormController implements Initializable{
 		}
 		try {
 			entity = getFormData();
+			
 			// SALVANDO NO BANCO DE DADOS
 			service.SaveOrUpdate(entity);
 			notifyDataChangeListeners();
+			
 			// FECHAR JANELA
 			Utils.currentStage(event).close();
-		} catch (DbException e) {
+			
+		} 	catch (ValidationException e) {
+			setErrorMessages(e.getErrors());
+		} 	catch (DbException e) {
 			Alerts.showAlert("Error saving objeect", null, e.getMessage(), AlertType.ERROR);
 		}
 	}
@@ -84,9 +92,20 @@ public class DepartmentFormController implements Initializable{
 	}
 
 	private Department getFormData() {
+		
 		Department obj = new Department();
 		
+		ValidationException exception = new ValidationException("Validation error");
+		
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "field can't be empty");
+		}
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		if(exception.getErrors().size() > 0) {
+			throw exception;
+		}
+		
 		obj.setName(txtName.getText());
 		
 		return obj;
@@ -112,6 +131,14 @@ public class DepartmentFormController implements Initializable{
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
+	}
+	
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if(fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
 	}
 	
 }
---
+++ src/model/exceptions/ValidationException.java
@@ -0,0 +1,26 @@
+package model.exceptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidationException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Map<String, String> errors = new HashMap<>();
+	
+	public ValidationException(String msg) {
+		super(msg);
+	}
+	
+	public Map<String, String> getErrors() {
+		return errors;
+	}
+	
+	public void addError(String fieldName, String errrorMessage) {
+		errors.put(fieldName, errrorMessage);
+	}
+	
+	
+
+}
