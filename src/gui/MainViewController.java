19110c9fb8bdac5f06dabdb877e35e707160a3fa
Joao Pedro <52724220+joomoraes@users.noreply.github.com>
2020-04-04 23:35:55 -0300

    Foram atribuidas as classes seller e atualizadas a view principal

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -17,6 +17,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import model.services.DepartmentServices;
+import model.services.SellerServices;
 
 public class MainViewController implements Initializable {
 
@@ -32,7 +33,11 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemSellerAction() {
-		System.out.println("onMenuItemSellerAction");
+		//EXPRESSÃO LAMBDÁ
+				loadView("/gui/SellerList.fxml", (SellerListController controller) ->  {
+					controller.setSellerService(new SellerServices());
+					controller.updateTableView();
+				});
 	}
 	
 	@FXML
